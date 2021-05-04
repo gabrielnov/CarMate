977249bb0127693449c51e21e4519af8049748b3
Gabriel <gc.novaes@outlook.com>
2021-05-04 18:51:57 -0300

    first commit

---
+++ src/test/java/br/com/carmate/Carmate/CarmateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carmate.Carmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
